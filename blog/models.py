ce4efcefff814b0053847b83f2e09adbcb134dab
Jaime Lovera <lovera.jaime@gmail.com>
2019-07-05 14:36:32 -0700

    the correct posts are shown on the category pages and on the homepage featured section. admin post model upload tool is working now and uploads it to my static folder.

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,25 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+import os
 
 
 class Post(models.Model):
-    broad_category = models.TextField()
-    specific_category = models.TextField()
-    is_featured = models.TextField()
+    categories = (
+        ('FRUGALITY', 'Frugality'),
+        ('INVESTING', 'Investing'),
+        ('CREDIT_CARDS', 'Credit Cards'),
+    )
+    featured = (
+        ('YES', 'Yes'),
+        ('NO', 'No'),
+    )
+
+    category = models.CharField(max_length=20, choices=categories)
+    is_featured = models.CharField(max_length=3, choices=featured, default='NO')
+    image= models.ImageField(upload_to="blog/static/blog/img", help_text="Image must be 1200 x 800")
     title = models.CharField(max_length=200)
+    body = models.TextField()
     published_date = models.DateTimeField(default=timezone.now)
 
     def publish(self):
--- blog/views.py
+++ blog/views.py
@@ -3,18 +3,18 @@ from django.utils import timezone
 from .models import Post
 
 def homepage(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(is_featured='YES').order_by('-published_date')
     return render(request, 'blog/homepage.html', {'posts': posts})
 
 def frugality(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(category='FRUGALITY').order_by('-published_date')
     return render(request, 'blog/frugality.html', {'posts': posts})
 
 def investing(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(category='INVESTING').order_by('-published_date')
     return render(request, 'blog/investing.html', {'posts': posts})
 
 def creditcards(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(category='CREDIT_CARDS').order_by('-published_date')
     return render(request, 'blog/creditcards.html', {'posts': posts})
 
