c0054808cd44abbe217db57c05a88b70b675c61e
Jaime Lovera <lovera.jaime@gmail.com>
2019-07-05 23:32:49 -0700

    added more columns to post in admin and added filters in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'is_featured', 'published_date')
+    list_filter = ('category', 'is_featured', 'published_date')
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -17,10 +17,10 @@ class Post(models.Model):
 
     is_featured = models.CharField(max_length=3, choices=featured, default='NO')
     category = models.CharField(max_length=20, choices=categories)
-    image= models.ImageField(upload_to="blog/static/blog/img", help_text="This is required and image must be 1200 x 800")
+    image= models.ImageField(upload_to="blog/static/blog/img", help_text="<h3>This is required and image must be 1200 x 800</h3>")
     title = models.CharField(max_length=200)
-    body_html = models.TextField(verbose_name="body (HTML)")
-    published_date = models.DateTimeField(default=None,blank=True, null=True)
+    body_html = models.TextField(verbose_name="body (HTML)", help_text="Use the following html tags: p, h1, h2 (paragrah, bold text, quote)")
+    published_date = models.DateTimeField(default=None,blank=True, null=True, help_text="Post will not be published until this field is set (Leave blank for draft)")
 
     def __str__(self):
         return self.title
