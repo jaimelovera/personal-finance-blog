9ac4a85e3c3c2d8eef1004911fa0d6120e908417
Jaime Lovera <lovera.jaime@gmail.com>
2019-07-08 19:35:47 -0700

    formatted the post detail page and added media queries

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 import os
+from django.core.exceptions import ValidationError
 
+def validate_image(image):
+    file_height = image.height 
+    file_width = image.width
+    if (file_width != 1200) or (file_height != 800):
+        raise ValidationError("Image must be 1200 x 800")
 
 class Post(models.Model):
     categories = (
@@ -17,9 +23,9 @@ class Post(models.Model):
 
     is_featured = models.CharField(max_length=3, choices=featured, default='NO')
     category = models.CharField(max_length=20, choices=categories)
-    image= models.ImageField(upload_to="blog/static/blog/img", help_text="<h3>This is required and image must be 1200 x 800</h3>")
+    image= models.ImageField(upload_to="blog/static/blog/img", validators=[validate_image], help_text="Image must be 1200 x 800")
     title = models.CharField(max_length=200)
-    body_html = models.TextField(verbose_name="body (HTML)", help_text="Use the following html tags: p, h1, h2 (paragrah, bold text, quote)")
+    body_html = models.TextField(verbose_name="body (HTML)", help_text="Use the following html tags:</br>h1:bold header</br>p:paragrah</br>h2:quote")
     published_date = models.DateTimeField(default=None,blank=True, null=True, help_text="Post will not be published until this field is set (Leave blank for draft)")
 
     def __str__(self):
