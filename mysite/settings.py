a22ed364e7c099907aa39fcf558f5828284fbacc
Jaime Lovera <lovera.jaime@gmail.com>
2019-07-13 20:47:42 -0700

    put note in setting file that that secret key is not the same one I will use in production

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9=v^2(#qmhfx9o2h0-3inkaz_iyi$y9hnx=s%dtnt5mla31uun'
+SECRET_KEY = '9=v^2(#qmhfx9o2h0-3inkaz_iyi$y9hnx=s%dtnt5mla31uun' #Not secret key used in production
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
