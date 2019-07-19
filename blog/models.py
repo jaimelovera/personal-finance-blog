3b587ddda926b6beffef079e8b9c0bc5547644c6
Jaime Lovera <lovera.jaime@gmail.com>
2019-07-19 15:49:16 -0700

    updated django admin for my post model helptext.

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ def validate_image(image):
     file_height = image.height 
     file_width = image.width
     if file_width/file_height != 3/2:
-        raise ValidationError("Image aspect ratio must be exactly 3:2")
+        raise ValidationError("Image aspect ratio must be exactly 3:2 i.e. 1200 x 800")
 
     #if (file_width != 1200) or (file_height != 800):
         #raise ValidationError("Image must be 1200 x 800")
@@ -27,7 +27,7 @@ class Post(models.Model):
 
     is_featured = models.CharField(max_length=3, choices=featured, default='NO')
     category = models.CharField(max_length=20, choices=categories)
-    main_image= models.ImageField(upload_to="blog/static/blog/img", validators=[validate_image], help_text="Required. Image must be 1200 x 800")
+    main_image= models.ImageField(upload_to="blog/static/blog/img", validators=[validate_image], help_text="Aspect ratio must be exactly 3:2 </br> i.e. 1200 x 800")
     inline_image_1= models.ImageField(upload_to="blog/static/blog/img", null=True, blank=True, default=None, help_text="Optional")
     inline_image_2= models.ImageField(upload_to="blog/static/blog/img", null=True, blank=True, default=None, help_text="Optional")
     inline_image_3= models.ImageField(upload_to="blog/static/blog/img", null=True, blank=True, default=None, help_text="Optional")
@@ -35,8 +35,8 @@ class Post(models.Model):
     inline_image_5= models.ImageField(upload_to="blog/static/blog/img", null=True, blank=True, default=None, help_text="Optional")
     inline_image_6= models.ImageField(upload_to="blog/static/blog/img", null=True, blank=True, default=None, help_text="Optional")
     title = models.CharField(max_length=200)
-    body_html = models.TextField(verbose_name="body (HTML)", help_text="Use the following tags between brackets:</br>h1 : bold header</br>p : paragrah</br>h2 : quote (keep between paragraphs for proper padding)</br>img1 - img6 : inline image")
-    published_date = models.DateTimeField(default=None,blank=True, null=True, help_text="Post will not be published until this field is set (Leave blank for draft)")
+    body_html = models.TextField(verbose_name="body (HTML)", help_text="<p> Use the following tags:</br> <xmp><h1>Bold Header</h1></xmp> <xmp><p>Paragraph</p></xmp> <xmp><h2>Quote</h2></xmp> <xmp><a href='url' target='_blank'>Link</a></xmp> <xmp><img1> - <img6></xmp> </p>")
+    published_date = models.DateTimeField(default=None,blank=True, null=True, help_text="Post will not be published until this field is set </br> *Leave blank for draft")
 
     def __str__(self):
         return self.title
