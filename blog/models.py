e480435882f8abcddc09699fbd3be870185ca6e3
Jaime Lovera <lovera.jaime@gmail.com>
2019-07-19 20:27:42 -0700

    added a post slug to the url for SEO reasons

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 import os
+from django.template.defaultfilters import slugify
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -34,9 +35,14 @@ class Post(models.Model):
     inline_image_4= models.ImageField(upload_to="blog/static/blog/img", null=True, blank=True, default=None, help_text="Optional")
     inline_image_5= models.ImageField(upload_to="blog/static/blog/img", null=True, blank=True, default=None, help_text="Optional")
     inline_image_6= models.ImageField(upload_to="blog/static/blog/img", null=True, blank=True, default=None, help_text="Optional")
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=300)
+    slug = models.SlugField(blank=True, editable=False)
     body_html = models.TextField(verbose_name="body (HTML)", help_text="<p> Use the following tags:</br> <xmp><h1>Bold Header</h1></xmp> <xmp><p>Paragraph</p></xmp> <xmp><h2>Quote</h2></xmp> <xmp><a href='url' target='_blank'>Link</a></xmp> <xmp><img1> - <img6></xmp> </p>")
     published_date = models.DateTimeField(default=None,blank=True, null=True, help_text="Post will not be published until this field is set </br> *Leave blank for draft")
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,6 @@ from django.shortcuts import get_object_or_404
 def not_found_404(request, exception):
     return render(request, 'blog/404.html', status=404)
 
-def subscribe(request):
-    return render(request, 'blog/subscribe.html')
-
 def contact(request):
     return render(request, 'blog/contact.html')
 
@@ -37,8 +34,8 @@ def creditcards(request):
     posts = Post.objects.filter(category='CREDIT_CARDS').exclude(published_date=None).order_by('-published_date')
     return render(request, 'blog/creditcards.html', {'posts': posts})
 
-def post_detail(request, pk):
+def post_detail(request, pk, slug):
     posts = Post.objects.exclude(published_date=None)
-    post = get_object_or_404(posts, pk=pk)
+    post = get_object_or_404(posts, pk=pk, slug=slug)
     return render(request, 'blog/post_detail.html', {'post': post})
 
