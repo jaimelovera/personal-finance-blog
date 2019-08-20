2ba253e3e3c613a1009a0aa644331c7013fe02b7
Jaime Lovera <lovera.jaime@gmail.com>
2019-08-19 18:11:03 -0700

    added loading icon and fadein into post detail images. made a fake temp div to take place of my main image on category pages so it takes up space and the top block is always that size even before the images load. looks cleaner and less choppy. fixed saving png image. was not centered.

--- blog/templatetags/textfield_img_tags.py
+++ blog/templatetags/textfield_img_tags.py
@@ -7,15 +7,15 @@ register = template.Library()
 @stringfilter
 def textfield_img_tags(value, post):
 	if post.inline_image_1:
-		value = value.replace("<img1>", "<img src=" + post.inline_image_1.url + ">")
+		value = value.replace("<img1>", "<div class='box-image-container'><img class='box-image' onload='fadeImageIn(this);'' src=" + post.inline_image_1.url + " alt='Posts Image'></div>")
 	if post.inline_image_2:
-		value = value.replace("<img2>", "<img src=" + post.inline_image_2.url + ">")
+		value = value.replace("<img2>", "<div class='box-image-container'><img class='box-image' onload='fadeImageIn(this);'' src=" + post.inline_image_2.url + " alt='Posts Image'></div>")
 	if post.inline_image_3:
-		value = value.replace("<img3>", "<img src=" + post.inline_image_3.url + ">")
+		value = value.replace("<img3>", "<div class='box-image-container'><img class='box-image' onload='fadeImageIn(this);'' src=" + post.inline_image_3.url + " alt='Posts Image'></div>")
 	if post.inline_image_4:
-		value = value.replace("<img4>", "<img src=" + post.inline_image_4.url + ">")
+		value = value.replace("<img4>", "<div class='box-image-container'><img class='box-image' onload='fadeImageIn(this);'' src=" + post.inline_image_4.url + " alt='Posts Image'></div>")
 	if post.inline_image_5:
-		value = value.replace("<img5>", "<img src=" + post.inline_image_5.url + ">")
+		value = value.replace("<img5>", "<div class='box-image-container'><img class='box-image' onload='fadeImageIn(this);'' src=" + post.inline_image_5.url + " alt='Posts Image'></div>")
 	if post.inline_image_6:
-		value = value.replace("<img6>", "<img src=" + post.inline_image_6.url + ">")
+		value = value.replace("<img6>", "<div class='box-image-container'><img class='box-image' onload='fadeImageIn(this);'' src=" + post.inline_image_6.url + " alt='Posts Image'></div>")
 	return value
