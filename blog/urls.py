bd68ec10f7cd95db5d891291ef09184912ca243f
Jaime Lovera <lovera.jaime@gmail.com>
2019-11-03 18:25:37 -0800

    added a tools link to the navigation bar. And set up environment to allow me to run a react app embedded into one of my templates. I tested this with another react app and all is working. The base template is extended and a div with id root is placed in the main block which holds the react app. Have to build the react app and copy the build folder to the money-vizualizer directory. Then take the scripts and linked css file from the index.html file and place them in the template I want to use the react app in.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views 
+from django.views.generic import TemplateView
 
 urlpatterns = [
 	path('', views.homepage, name='homepage'),
@@ -7,11 +8,14 @@ urlpatterns = [
 	path('investing', views.investing, name='investing'),
 	path('saving', views.saving, name='saving'),
 	path('creditcards', views.creditcards, name='creditcards'),
+	path('tools', views.tools, name='tools'),
+	path('money-visualizer', TemplateView.as_view(template_name='blog/money_visualizer.html'), name='money-visualizer'),
 	path('post/<int:pk>/<slug:slug>', views.post_detail, name='post-detail'),
 	path('subscribe-thank-you', views.subscribe_thank_you, name='subscribe-thank-you'),
 	path('legal', views.legal, name='legal'),
 	path('contact', views.contact, name='contact'),
 	path('about', views.about, name='about'),
 	path('search', views.search, name='search'),
+
 ]
 
