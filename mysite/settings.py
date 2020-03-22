0361ec26993d639c9602e749df1b7a0a6856fa7e
Jaime Lovera <lovera.jaime@gmail.com>
2020-03-22 04:22:08 -0700

    updating allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9=v^2(#qmhfx9o2h0-3inkaz_iyi$y9hnx=s%dtnt5mla31uun' #Not secret ke
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
